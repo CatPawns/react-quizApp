6837cbddc3ab08e75df167613830c6cf75bf8c0e
CatPawns <juanmeza8264@gmail.com>
2022-01-30 18:21:46 -0500

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -22,19 +22,37 @@ class question
     return this.question;
   }
 }
+function createNewQuestion(a,b)
+{
+  
+}
 export default function App() {
-  var set1 = new question("sos subnormal?","si");
-  var set2 = new question("te gustan las pijas", "no");
-  var set3 = new question("1+1=2",2);
+  var set1 = new question("sos humano?","si");
+  var set2 = new question("te gustan las #var", "no");
+  var set3 = new question("1+1=",2);
+  var set3 = new question("ま","ma");
+  var questionSet = [set1,set2,set3,2]
   return (
     
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Quiz app for learning!</h1>
+      <p> :)</p>
       
       <div>
     <h2> Preguntas</h2>
+    {
+    
+    //for loop
+      questionSet.forEach(function(elemento, indice, array) {
+
+        console.log(elemento, indice); })
+
+      }
     <p>{set1.Answer}</p>
+      <input></input>
+    <button>
+            submit
+          </button>
     </div>
 
     </div>
