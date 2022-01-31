45acf6c26d7bb80c074f85a387cd0641c0d7ba83
CatPawns <juanmeza8264@gmail.com>
2022-01-31 03:13:45 -0500

    3 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,62 +1,85 @@
-import React from "react";
-import "./style.css";
-class question
-{
-  constructor(answer,question)
-  {
+import React from 'react';
+import './style.css';
+
+class question {
+  constructor(answer, question) {
     this.answer = answer;
     this.question = question;
   }
-  get Answer(){
+  get Answer() {
     return this.answer;
   }
-  get Question()
-  {
+  get Question() {
     return this.question;
   }
-  calcAnswer(){
+  calcAnswer() {
     return this.answer;
   }
-  calcQuestion()
-  {
+  calcQuestion() {
     return this.question;
   }
 }
-function createNewQuestion(a,b)
-{
+//random numbers
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+var getRand = (function () {
+  var nums = [0, 1, 2, 3];
+  var current = [];
+  //*
+  function rand(n) {
+    return (Math.random() * n) | 0;
+  }
   
+  return function () {
+    if (!current.length) current = nums.slice();
+    return current.splice(rand(current.length), 1);
+  };
+})();
+var currentQuestion = 0;
+
+function advance(){
+  currentQuestion += 1;
+  console.log("NEXT QUESTION");
 }
+
+//end random numbers
 export default function App() {
-  var set1 = new question("sos humano?","si");
-  var set2 = new question("te gustan las #var", "no");
-  var set3 = new question("1+1=",2);
-  var set3 = new question("ま","ma");
-  var questionSet = [set1,set2,set3,2]
+  var set1 = new question('sos humano?', 'si');
+  var set2 = new question('te gustan las #var', 'no');
+  var set3 = new question('ま ', "ma");
+  var set4 = new question('き', "ki");
+  var questionSet = [set1, set2, set3, set4, 2];
+  let RN = getRand();
+
+  var currentAsnwer = questionSet[RN];
+  var el = document.getElementById("b1");
+  el.addEventListener("click",advance,false);
+  
   return (
-    
     <div>
       <h1>Quiz app for learning!</h1>
       <p> :)</p>
-      
+
       <div>
-    <h2> Preguntas</h2>
-    {
-    
-    //for loop
-      questionSet.forEach(function(elemento, indice, array) {
+        <h2> Preguntas</h2>
+        {
+          
+          //for loop
+          questionSet.forEach(function (elemento, indice, array) {
+            //console.log(elemento, indice);
+          })
+          
+        }
+        
 
-        console.log(elemento, indice); })
-
-      }
-    <p>{set1.Answer}</p>
-      <input></input>
-    <button>
-            submit
-          </button>
+        <p>{currentAsnwer.Answer}</p>
+        <input></input>
+        
+      </div>
     </div>
-
-    </div>
-
-    
   );
+  
+
+  
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
+import Toggle from"./Toggle";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Toggle />,document.getElementById("test2"));
+class menu extends React.Component {
+  render() {
+     return (
+        <div>
+           <ul>
+           <li>Home</li>
+           <li>About</li>
+           <li>Contact</li>
+           </ul>
+           {this.props.children}
+        </div>
+     )
+  }
+}
+
